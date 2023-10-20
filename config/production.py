314d83d34ba064e97a81a8c38a003378c830c3ab
은지 <dmswl6842@naver.com>
2023-10-20 23:15:06 +0900

    ?

--- config/production.py
+++ config/production.py
@@ -3,7 +3,7 @@ from logging.config import dictConfig
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = bb'.\xa2\xe1\xd9\xd1\xd10\x16\xb7\xe3\xcf\xab\xb2r\xe2\x0f'
+SECRET_KEY = b'.\xa2\xe1\xd9\xd1\xd10\x16\xb7\xe3\xcf\xab\xb2r\xe2\x0f'
 
 dictConfig({
     'version': 1,
